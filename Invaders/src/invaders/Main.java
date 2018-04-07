37be2e799780698ba66981ae94f27b465eb382de
karlami <karla.rivera24@gmail.com>
2018-04-07 01:39:28 -0600

    Creadas las opciones de visualizacion basicas de la ventana

---
+++ Invaders/src/invaders/Main.java
@@ -0,0 +1,29 @@
+
+package invaders;
+
+import java.awt.Canvas;
+import javax.swing.JFrame;
+
+public class Main extends Canvas implements Runnable{
+
+    public static void main(String[] args) {
+        Main display = new Main();
+        JFrame frame = new JFrame();
+        frame.add(display);
+        frame.pack();
+        frame.setTitle("Invaders");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        
+    }
+    public Main(){
+        
+    }
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
