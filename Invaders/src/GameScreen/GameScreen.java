3aed125d33ffa890d7d24f928b6ab133efea8c42
karlami <karla.rivera24@gmail.com>
2018-04-07 16:47:55 -0600

    Creada, para que sea la pantalla de juego donde el usuario interactua con la nave

---
+++ Invaders/src/GameScreen/GameScreen.java
@@ -0,0 +1,21 @@
+
+package GameScreen;
+
+import State.SuperStateMachine;
+import java.awt.Canvas;
+import java.awt.Graphics2D;
+
+public class GameScreen implements SuperStateMachine{
+
+    @Override
+    public void update(double delta) {}
+
+    @Override
+    public void draw(Graphics2D g) {
+    }
+
+    @Override
+    public void init(Canvas canvas) {}
+    
+    
+}
