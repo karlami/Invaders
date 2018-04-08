f8fd06a57efbe49035db81cc0c5b04975d6c8149
karlami <karla.rivera24@gmail.com>
2018-04-08 00:25:31 -0600

    Creacion de clase Timer que retorna true si alcanza su valor

---
+++ Invaders/src/Timer/Timer.java
@@ -0,0 +1,40 @@
+
+package Timer;
+
+public class Timer {
+    private long prevTime;
+    
+    public Timer(){
+        prevTime = System.currentTimeMillis();
+    }
+    public void resetTimer(){
+	prevTime = System.currentTimeMillis();
+    }
+    
+    public boolean timerEvent(int timer){
+	if(System.currentTimeMillis() - getPrevTime() > timer){
+            resetTimer();
+            return true;
+	}
+		
+	return false;
+    }
+    
+    public boolean isTimerReady(int timer){
+	if(System.currentTimeMillis() - getPrevTime() > timer){
+            return true;
+	}
+		
+	return false;
+    }
+    
+    public long getPrevTime() {
+        return prevTime;
+    }
+
+    public void setPrevTime(long prevTime) {
+        this.prevTime = prevTime;
+    }
+    
+    
+}
