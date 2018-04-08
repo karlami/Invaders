f41b2cfc3bd00bf8e2d2ded26080a6947b21b17c
karlami <karla.rivera24@gmail.com>
2018-04-08 03:21:47 -0600

    Crea la bala a partir de un rectangulo, lo dibuja anaranjado, lo actualiza y le cambia su pos Y de acuerdo a su velocidad, usa el metodo intercects() de la clase Rectangulo para ver cuando dos rectangulos intersecan y hacer la colision, los enemigos deben ser rectangulos tambien, los destruye y desaparece cuando estan fuera de pantalla

---
+++ Invaders/src/Bullets/MachineGun.java
@@ -0,0 +1,79 @@
+
+package Bullets;
+
+import Main.Main;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
+public class MachineGun extends PlayerWeaponType{
+ 
+	private Rectangle bullet;
+	private final double speed = 2.5d;
+	
+	public MachineGun(double xPos, double yPos, int width,int height){
+		this.setxPos(xPos);
+		this.setyPos(yPos);
+		this.setWidth(width);
+		this.setHeight(height);
+		
+		this.bullet = new Rectangle((int) getxPos(),(int) getyPos(), getWidth(), getHeight());
+	}
+	
+	@Override
+	public void draw(Graphics2D g) {
+            if(bullet == null)
+                return;
+
+            g.setColor(Color.ORANGE);
+            g.fill(bullet);
+	}
+
+	@Override
+	public void update(double delta) {
+            if(bullet == null)
+                return;
+
+            this.setyPos(getyPos() - (delta * speed));
+            bullet.y = (int) this.getyPos();
+            isOutofBounds();
+	}
+        
+        /**
+         * Cuando colisionan dos rectangulos, se da una interseccion entre ambos
+         * @param rect que va a ser con el que va a chocar
+         * @return true si intersecan, false si no
+         */
+
+	@Override
+	public boolean collisionRect(Rectangle rect) {
+            if(this.bullet == null)
+                return false;
+
+            if(bullet.intersects(rect)){
+                this.bullet = null;
+                return true;
+            }
+
+            return false;
+	}
+
+	@Override
+	public boolean destroy() {
+            if(bullet == null)
+                return true;
+
+            return false;
+	}
+
+	@Override
+	protected void isOutofBounds() {
+            if(this.bullet == null)
+                return;
+
+            if(bullet.y < 0 || bullet.y > Main.HEIGHT || bullet.x < 0 || bullet.x > Main.WIDTH){
+                bullet = null;
+            }
+	}
+
+}
