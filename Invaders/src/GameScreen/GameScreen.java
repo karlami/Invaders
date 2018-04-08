005881d9760cb9ff94a8a63066ef08d63b6f04b8
karlami <karla.rivera24@gmail.com>
2018-04-07 19:51:11 -0600

    Se agrega un player a la pantalla del juego, con la imagen y una posicion provisional

--- Invaders/src/GameScreen/GameScreen.java
+++ Invaders/src/GameScreen/GameScreen.java
@@ -6,16 +6,27 @@ import java.awt.Canvas;
 import java.awt.Graphics2D;
 
 public class GameScreen implements SuperStateMachine{
-
-    @Override
-    public void update(double delta) {}
-
-    @Override
-    public void draw(Graphics2D g) {
+    
+    private Player player;
+    
+    public GameScreen(){
+        player = new Player(150,150,50,40);
     }
 
     @Override
-    public void init(Canvas canvas) {}
+    public void update(double delta) {
+        player.update(delta);
+    }
+
+    @Override
+    public void draw(Graphics2D g) {
+        player.draw(g);
+    }
+
+    @Override
+    public void init(Canvas canvas) {
+        canvas.addKeyListener(player);
+    }
     
     
 }
