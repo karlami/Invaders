a6d830cebfe8a28cd5fc37c4e0653c214928189c
karlami <karla.rivera24@gmail.com>
2018-04-11 12:27:13 -0600

    Se agrega level1

--- Invaders/src/GameScreen/GameScreen.java
+++ Invaders/src/GameScreen/GameScreen.java
@@ -1,6 +1,7 @@
 
 package GameScreen;
 
+import Levels.Level1;
 import State.SuperStateMachine;
 import java.awt.Canvas;
 import java.awt.Graphics2D;
@@ -9,18 +10,25 @@ public class GameScreen implements SuperStateMachine{
     
     private Player player;
     
+    private Level1 level;
+    
     public GameScreen(){
-        player = new Player(375,550,50,40);
+       player = Player.getPlayer(375,550,50,40);
+       
+       
+       level = new Level1(player);
     }
 
     @Override
     public void update(double delta) {
         player.update(delta);
+        level.update(delta);
     }
 
     @Override
     public void draw(Graphics2D g) {
         player.draw(g);
+        level.draw(g);
     }
 
     @Override
