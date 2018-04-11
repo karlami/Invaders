bfb1451ed16ac160edf67f13d7d2a9bb282674ab
karlami <karla.rivera24@gmail.com>
2018-04-11 12:34:09 -0600

    Ahora se encarga de la visualizacion de los sprites directamente, se cambia el tamaño y la imagen y se crea limit que nos permite la indexacion de los elementos de la lista de enemigos

--- Invaders/src/Sprite/SpriteAnimation.java
+++ Invaders/src/Sprite/SpriteAnimation.java
@@ -3,9 +3,11 @@ package Sprite;
 
 import EstructurasDatos.ListaSimple;
 import Timer.Timer;
-import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
 
 public class SpriteAnimation {
     
@@ -20,22 +22,41 @@ public class SpriteAnimation {
     private Timer timer;
     
     private int animationSpeed;
+    private int width, height;
+    private int limit;
     
     private double posX;
     private double posY;
     
-    public SpriteAnimation(double posX, double posY, int animationSpeed){
+    public SpriteAnimation(double posX, double posY,int rows, int columns, int animationSpeed, String imgPath){
         this.animationSpeed = animationSpeed;
         this.posX = posX;
         this.posY = posY;
         
+        try{
+                URL url = this.getClass().getResource(imgPath);
+                BufferedImage pSprite = ImageIO.read(url);
+                int spriteWidth = pSprite.getWidth() / columns;
+                int spriteHeight = pSprite.getHeight() / rows;
+                for(int y = 0; y < rows; y++){
+                    for(int x = 0; x < columns; x++)
+                        addSprite(pSprite
+                                , 0 + (x * spriteWidth)
+                                , 0 + (y * spriteHeight)
+                                , spriteWidth
+                                , spriteHeight);
+                }
+            }catch(IOException e){};
+        
         timer = new Timer();
+        // pues el tamaño no incluye al cero indice
+        limit = sprites.getSize() - 1;
     }
     
     public void draw(Graphics2D g){
         if (isSpriteAnimDestroyed())
             return;
-        g.drawImage(sprites.get(currentSprite), (int)getPosX(), (int)getPosY(), null);
+        g.drawImage(sprites.get(currentSprite), (int)getPosX(), (int)getPosY(), width, height, null);
     }
     
     public void update(double delta){
@@ -61,22 +82,22 @@ public class SpriteAnimation {
     
     
     private void loopAnimation() {
-	if (timer.isTimerReady(animationSpeed) && currentSprite == sprites.getSize()-1){
+	if (timer.isTimerReady(animationSpeed) && currentSprite == limit){
             currentSprite = 0;
             timer.resetTimer();
-	}else if (timer.timerEvent(animationSpeed) && currentSprite != sprites.getSize()-1) {
+	}else if (timer.timerEvent(animationSpeed) && currentSprite != limit) {
             currentSprite++;
 	} 
     }
 
     
     private void playAnimation() {
-	if (timer.isTimerReady(animationSpeed) && currentSprite != sprites.getSize()-1 && !getDestroyAfterAnim()) {
+	if (timer.isTimerReady(animationSpeed) && currentSprite != limit && !getDestroyAfterAnim()) {
             play = false;
             currentSprite = 0;
-	} else if (timer.isTimerReady(animationSpeed) && currentSprite == sprites.getSize()-1 && getDestroyAfterAnim()) {
+	} else if (timer.isTimerReady(animationSpeed) && currentSprite == limit && getDestroyAfterAnim()) {
             sprites = null;
-	}else if (timer.timerEvent(animationSpeed) && currentSprite != sprites.getSize()-1) {
+	}else if (timer.timerEvent(animationSpeed) && currentSprite != limit) {
             currentSprite++;
 	}
     }
@@ -95,7 +116,10 @@ public class SpriteAnimation {
         sprites.add(spriteMap.getSubimage(posX, posY, width, height));
 	}
     
-    public void playerAnimation(boolean play, boolean destroyAfterAnim){
+    public void setPlay(boolean play, boolean destroyAfterAnim){
+        if(loop){
+            loop = false;
+        }
         this.play = play;
         this.destroyAfterAnim = destroyAfterAnim;
     }
@@ -139,9 +163,59 @@ public class SpriteAnimation {
     public void setLoop(boolean loop) {
         this.loop = loop;
     }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public boolean isPlay() {
+        return play;
+    }
+
+    public void setPlay(boolean play) {
+        this.play = play;
+    }
+
+    public int getAnimationSpeed() {
+        return animationSpeed;
+    }
+
+    public void setAnimationSpeed(int animationSpeed) {
+        this.animationSpeed = animationSpeed;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+    /**
+     * Para el tamaño de la lista - 1 por los indices, pues empiezan en 0
+     * @param limit que va usarse para mandar el tamaño de la lista segun la cantidad 
+     * que haya, si es 0 cambia el valor por el solicitado tal y como lo solicita
+     */
+
+    public void setLimit(int limit) {
+        if(limit > 0){
+            this.limit = limit - 1;
+        }else{
+            this.limit = limit;
+        }
+    }
     
+    public void resetLimit(){
+        limit = sprites.getSize() - 1;
+    }
+
+    }
     
-    
-    
-    
-}
