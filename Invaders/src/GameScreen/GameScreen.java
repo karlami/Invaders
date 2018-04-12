039595acf22fd5ef8bbf7c74d4bd1999847de736
karlami <karla.rivera24@gmail.com>
2018-04-11 21:35:59 -0600

    Hay un nivel completo cada vez que termina la hilera, esto debe ser cada 3 hileras diferentes

--- Invaders/src/GameScreen/GameScreen.java
+++ Invaders/src/GameScreen/GameScreen.java
@@ -2,12 +2,16 @@
 package GameScreen;
 
 import Levels.Level1;
+import State.StateMachine;
 import State.SuperStateMachine;
 import java.awt.Canvas;
+import java.awt.Font;
+import Timer.TickTimer;
 import java.awt.Color;
 import java.awt.Graphics2D;
+import Main.Main;
 
-public class GameScreen implements SuperStateMachine{
+public class GameScreen extends SuperStateMachine{
     
     private Player player;
     
@@ -15,30 +19,83 @@ public class GameScreen implements SuperStateMachine{
     
     public static int score = 0;
     
-    public GameScreen(){
+    private Font gameScreen = new Font("Arial", Font.PLAIN, 48);
+    
+    private TickTimer gameOverTimer = new TickTimer(180);
+    private TickTimer completeTimer = new TickTimer(180);
+     
+    public GameScreen(StateMachine stateMachine){
+       super(stateMachine);
+       
        player = Player.getPlayer(375,550,50,40);
        level = new Level1(player);
     }
+
+    
 
     @Override
     public void update(double delta) {
         player.update(delta);
         level.update(delta);
+
+        if (level.isGameOver()) {
+            gameOverTimer.tick(delta);
+            if (gameOverTimer.isEventReady()) {
+                level.reset();
+                getStateMachine().setState((byte) 0);
+                score = 0;
+            }
+        }
+
+        if (level.isComplete()) {
+            completeTimer.tick(delta);
+            if (completeTimer.isEventReady()) {
+                level.reset();
+            }
+        }
     }
 
     @Override
     public void draw(Graphics2D g) {
         g.setColor(Color.white);
         g.drawString("Score: " + score, 5, 15);
-        
+
+        g.setColor(Color.red);
+        g.drawString("Health: " + player.getHealth(), 5, 35);
+
         player.draw(g);
         level.draw(g);
+
+        if (level.isGameOver()) {
+            g.setColor(Color.red);
+            g.setFont(gameScreen);
+            String gameOver = "GAME OVER!";
+            int gameOverWidth = g.getFontMetrics().stringWidth(gameOver);
+            g.drawString(gameOver, (Main.getWIDTH()/2)-(gameOverWidth/2), Main.getHEIGHT()/2);
+        }
+
+        if (level.isComplete()) {
+            g.setColor(Color.green);
+            g.setFont(gameScreen);
+            String complete = "LEVEL COMPLETE!";
+            int completeWidth = g.getFontMetrics().stringWidth(complete);
+            g.drawString(complete, (Main.getWIDTH()/2)-(completeWidth/2), Main.getHEIGHT()/2);
+        }
     }
 
     @Override
     public void init(Canvas canvas) {
         canvas.addKeyListener(player);
     }
+
+    public static int getScore() {
+        return score;
+    }
+
+    public static void setScore(int score) {
+        GameScreen.score = score;
+    }
     
     
+
 }
