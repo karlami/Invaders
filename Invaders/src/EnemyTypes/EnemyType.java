1ce7d2751af59b8bc631125fb9ddb5b9acb23209
karlami <karla.rivera24@gmail.com>
2018-04-20 13:10:24 -0600

    metodo collide con lista doble y posicPlayer para cuando este al nivel del jugador sea game over

--- Invaders/src/EnemyTypes/EnemyType.java
+++ Invaders/src/EnemyTypes/EnemyType.java
@@ -1,21 +1,24 @@
 
 package EnemyTypes;
 
+import EstructurasDatos.ListaDoble;
 import EstructurasDatos.ListaSimple;
 import GameScreen.Player;
 import java.awt.Graphics2D;
-
+/**
+ * Molde para los tipos de enemigos
+ */
 public interface EnemyType {
-	
-
+    
     public void draw(Graphics2D g);
     public void update(double delta, Player player);
     public void changeDirection(double delta);
 
     public boolean deathScene();
     public boolean collide(int i, Player player, ListaSimple<EnemyType> enemys);
+    public boolean collide(int i, Player player, ListaDoble<EnemyType> enemys);
     public boolean isOutOfBounds();
+    public boolean posicPlayer();
 
-    
     }
 
